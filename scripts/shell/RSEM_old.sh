3a8377431124a0ec2ebaabb03dce99504d42afdc
adaugb18 <adaugb18@student.aau.dk@a512-gc1-e-01.srv.aau.dk>
2023-05-15 09:46:32 +0200

    commit test

--- scripts/shell/RSEM_old.sh
+++ scripts/shell/RSEM_old.sh
@@ -8,3 +8,5 @@ parallel -j10 rsem-calculate-expression --paired-end --bowtie2 --bowtie2-sensiti
 /projects/glomicave/metatranscriptomics/01_mRNA_processed/processing_ribodetector/mRNA/{}_R2.fq \
 /user_data/ahd/EPS_PIPELINE/data/metatranscriptomics/RSEM_old/reference/combined.ORFs \
 /user_data/ahd/EPS_PIPELINE/data/metatranscriptomics/RSEM_old/results/{}
+
+#test
