18f3a56b9b0895339e13ad1da249f17d18a95e4b
adaugb18 <adaugb18@student.aau.dk@axomamma.srv.aau.dk>
2023-06-22 00:05:54 +0200

    lol

--- scripts/R/MAG_TPM_calculator.R
+++ scripts/R/MAG_TPM_calculator.R
@@ -2,12 +2,13 @@
 library(readr)
 library(tidyverse)
 
-files <- list.files("/user_data/ahd/EPS_PIPELINE/data/metatranscriptomics/RSEM_new/results", pattern = "*.genes.results")
+files <- list.files("/mnt/ahd/EPS_PIPELINE/data/metatranscriptomics/RSEM_new/results", pattern = "*.genes.results")
 
 for (file in files) {
-    temp <- read_tsv(paste0("/user_data/ahd/EPS_PIPELINE/data/metatranscriptomics/RSEM_new/results", file)) %>% 
-    mutate(MAG = substring(gene_id, 1, nchar(gene_id) - 6)) %>% group_by(MAG) %>% 
-    summarise(gene_id = gene_id, MAG_TPM = ((effective_count/effective_length)/(sum(effective_count/effective_length)))*1000000) %>%
-    write_tsv(paste0("/user_data/ahd/EPS_PIPELINE/data/metatranscriptomics/RSEM_new/results/MAG_TPM_files/", file))
+    temp <- read_tsv(paste0("/mnt/ahd/EPS_PIPELINE/data/metatranscriptomics/RSEM_new/results/", file)) %>%
+    filter(effective_length > 0) %>%
+    mutate(MAG = substring(gene_id, 1, nchar(gene_id) - 6), RKM = expected_count/effective_length) %>% group_by(MAG) %>% 
+    summarise(gene_id = gene_id, TPM = TPM, MAG_TPM = ((expected_count/effective_length)/sum(RKM))*1000000) %>%
+    write_tsv(temp, paste0("/mnt/ahd/EPS_PIPELINE/data/metatranscriptomics/RSEM_new/results/MAG_TPM_files/", file))
     print(file)
 }
