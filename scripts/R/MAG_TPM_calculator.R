53079eda03836b5f2ea6626c1c6d41d46bbd1ffd
Anders OHD <ahdhtxy14@gmail.com>
2023-06-22 00:04:13 +0200

    data.table calculator

--- scripts/R/MAG_TPM_calculator.R
+++ scripts/R/MAG_TPM_calculator.R
@@ -1,4 +1,4 @@
-
+library(data.table)
 library(readr)
 library(tidyverse)
 
@@ -12,4 +12,13 @@ for (file in files) {
     reframe(gene_id = gene_id, TPM = TPM, MAG_TPM = ((expected_count/effective_length)/(sum(RKM)))*1000000) %>%
     write_tsv(paste0("/mnt/ahd/EPS_PIPELINE/data/metatranscriptomics/RSEM_new/MAG_TPM_files/", file))
     print(file)
+}
+
+for (file in files) {
+  temp <- fread(paste0("/mnt/ahd/EPS_PIPELINE/data/metatranscriptomics/RSEM_new/results/", file))
+  temp[, MAG := substring(gene_id, 1, nchar(gene_id) - 6)]
+  temp[, RKM := ifelse(effective_length == 0, 0, expected_count / effective_length)]
+  temp[, MAG_TPM := ((expected_count / effective_length) / sum(RKM)) * 1000000, by = MAG]
+  fwrite(temp, paste0("/mnt/ahd/EPS_PIPELINE/data/metatranscriptomics/RSEM_new/MAG_TPM_files/", file))
+  print(file)
 }
